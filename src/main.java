920b773861ac19f2031d18428626c286dc101002
Nifgard <nifgard@o2.pl>
2016-04-16 18:28:41 +0200

    added ball class

--- src/main.java
+++ src/main.java
@@ -1,10 +1,30 @@
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.RenderingHints;
+
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
 
+
+
 public class main extends JPanel {
 	
-	public static void main(String[] args){
+	rock r = new rock(this);
+	
+	private void move(){
+		r.move();
+	}
+	
+	public void paint(Graphics g){
+		super.paint(g);
+		Graphics2D g2d = (Graphics2D) g;
+		g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+		r.paint(g2d);
+		
+	}
+	
+	public static void main(String[] args) throws InterruptedException{
 		JFrame frame = new JFrame("Looper");
 		main game = new main();
 		frame.add(game);
@@ -14,9 +34,9 @@ public class main extends JPanel {
 		boolean isRunning = true;
 		
 		while(isRunning){
-			
-			
+			game.move();
 			game.repaint();
+			Thread.sleep(10);
 			
 		}
 		
--- src/rock.java
+++ src/rock.java
@@ -1,14 +1,37 @@
 import java.awt.Graphics;
 
+import java.awt.Graphics2D;
+
 public class rock {
 
 	int x = 0;
 	int y = 0;
+	int xa = 1;
+	int ya = 1;
 	
-	super.paint(g);
-	Graphics2D g2d = (Graphics2D) g;
+	private main game;
 	
+	public rock(main game){
+		this.game= game;
+	}
 	
-	g2d.fillOval(30, 30, 50, 50);
+	void move(){
+		if(x + xa < 0)
+			xa = 1;
+		if (x + xa > game.getWidth() - 30)
+			xa = -1;
+		if(y + ya < 0)
+			ya = 1;
+		if(y + ya > game.getHeight() - 30)
+			ya = -1;
+		
+		x = x + xa;
+		y = y + ya;
+	}
+
+	public void paint(Graphics2D g) {
+		g.fillOval(x, y, 30, 30);
+	}
+	
 	
 }
